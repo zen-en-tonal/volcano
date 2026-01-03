d5ea922e62d6673edb9f2834c3c85d7597491601
Takeru KODAMA <zen.en.tonal@gmail.com>
2026-01-03 11:39:35 +0900

    Chore: tidy imports

--- src/visualiser/cava.rs
+++ src/visualiser/cava.rs
@@ -2,7 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use std::{error::Error, ffi::CStr, fmt::Display, mem};
+use std::{error::Error, ffi::CStr, fmt::Display};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
